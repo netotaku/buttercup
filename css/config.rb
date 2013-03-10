41859255c0c90d223afc6c70f8da3d5b513b9cb0
Nick Clarkson <orko@Nicks-MacBook-Pro.local>
2013-03-10 21:42:44 +0000

    compass stuff

---
+++ css/config.rb
@@ -0,0 +1,12 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+sass_dir = "scss"
+images_dir = "../images"
+javascripts_dir = "../js"
+css_dir = "./"
+output_style = :expanded
+sass_options = {:debug_info => true}
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+line_comments = true
